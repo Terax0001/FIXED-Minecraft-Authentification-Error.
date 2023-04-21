bbc100ac95ff7127492d8a306eedd348853937ea
Terax0002 <130836248+Terax0002@users.noreply.github.com>
2023-04-21 17:16:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from colorama import Back, Fore, Style, deinit, init
 import shutil
+import msvcrt as m
 init()
 
-input(Fore.MAGENTA + Style.NORMAL + 'Make Sure You Have The .minecraft Folder In Roaming. Type Enter To Continue\n')
+print(Fore.MAGENTA + Style.NORMAL + 'Make Sure You Have The .minecraft Folder In Roaming. Type Enter To Continue')
+m.getch()
 
 shutil.move(r'C:\Windows\System32\drivers\etc\hosts.ics', r'C:\Users\pc\AppData\Roaming\.minecraft\hosts.ics')
